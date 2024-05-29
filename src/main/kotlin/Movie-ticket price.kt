c6ecca49881f4c15966531fdb3fb1e9f13babc65
Juan Galicia <68486813+Juancho2004gg@users.noreply.github.com>
2024-05-28 21:44:52 -0400

    Exercise 2 change

--- src/main/kotlin/Movie-ticket price.kt
+++ src/main/kotlin/Movie-ticket price.kt
@@ -6,14 +6,14 @@ In the initial code provided in the following code snippet, write a program that
 A children's ticket price of $15 for people 12 years old or younger.
 A standard ticket price of $30 for people between 13 and 60 years old. On Mondays, discount the standard ticket price to $25 for this same age group.
 A senior ticket price of $20 for people 61 years old and older. Assume that the maximum age of a moviegoer is 100 years old.
-A -1 value to indicate that the price is invalid when a user inputs an age outside of the age specifications.
+A -1 value to indicate that the price is invalid when a user inputs an age outside the age specifications.
 * */
 
 /*
 The movie ticket price for a person aged 5 is $15.
 The movie ticket price for a person aged 28 is $25.
 The movie ticket price for a person aged 87 is $20.
-* */
+*/
 
 fun main() {
     val child = 5
@@ -28,22 +28,21 @@ fun main() {
 }
 
 fun ticketPrice(age: Int, isMonday: Boolean): Int {
-    var childrenTicketPrice: Int = 15
-    var standardTicketPrice: Int = 30
-    var seniorTicketPrice: Int = 20
-    var mondayDiscount: Int = 5  // 5 dollars
+    val childrenTicketPrice = 15
+    val standardTicketPrice = 30
+    val seniorTicketPrice = 20
+    val mondayDiscount = 5  // 5 dollars
 
-    if(age in 1..12){
-        return childrenTicketPrice
+    return if(age in 1..12){
+        childrenTicketPrice
     }
     else if(age in 13..60){
-        return if (isMonday) standardTicketPrice - mondayDiscount else standardTicketPrice
-
+        if (isMonday) standardTicketPrice - mondayDiscount else standardTicketPrice
     }
-    else if(age > 60){
-        return seniorTicketPrice
+    else if(age in 61..100){
+        seniorTicketPrice
     }
     else{
-        return -1
+        -1
     }
 }
